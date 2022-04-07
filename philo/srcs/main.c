417335f1f982f8b511811c896fb3cd18dd547853
Claudine Vine <cvine@si-b4.kzn.21-school.ru>
2022-04-07 13:23:04 +0300

    finished

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cvine <cvine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 18:55:32 by cvine             #+#    #+#             */
-/*   Updated: 2022/04/07 13:14:59 by cvine            ###   ########.fr       */
+/*   Updated: 2022/04/07 13:22:57 by cvine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	main(int argc, char **argv)
 {
-	
 	int		*int_argv;
 
 	int_argv = parse(argc, argv);
@@ -22,11 +21,5 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	if (!initialize(argc, int_argv))
 		return (EXIT_FAILURE);
-	// if (!philo)
-	// 	return (free_mem(philo, param, NULL, int_argv));
-	// printf(RED"Simulation started\n"COLOR_RESET);
-	// if (create_threads(philo, philo->param->num_of_philos))
-	// 	return (EXIT_FAILURE);
-	// printf(RED"Simulation stopped"COLOR_RESET);
 	return (EXIT_SUCCESS);
 }
